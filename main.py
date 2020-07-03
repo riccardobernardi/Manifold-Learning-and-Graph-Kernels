d5e44ed36ff3eb8cd25898e07b9f10cb4d0bed69
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-07-03 11:29:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from grakel.kernels import ShortestPath
 from grakel.kernels import NeighborhoodSubgraphPairwiseDistance
 from grakel.kernels import WeisfeilerLehman
 from grakel.kernels.graphlet_sampling import GraphletSampling
+from grakel.kernels.random_walk import RandomWalk
 
 
 launch(ShortestPath)
 launch(NeighborhoodSubgraphPairwiseDistance)
 launch(WeisfeilerLehman)
 launch(GraphletSampling)
+launch(RandomWalk)
 
