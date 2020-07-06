4557ac92a8cf42f19291031da2b4079f1a93d6b2
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-07-06 10:17:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from utils import results_PPI
 from utils import results_SHOCK
+import pandas as pd
 
 
 import PPI_SHOCK_SPK
@@ -9,5 +10,9 @@ import PPI_SHOCK_SPK
 # import PPI_LLE
 import DSGK
 
+results_PPI = pd.DataFrame(results_PPI,columns=["method","PPI_score"])
+results_SHOCK = pd.DataFrame(results_SHOCK,columns=["method","SHOCK_score"])
+
+results_PPI["SHOCK_score"] = results_SHOCK["SHOCK_score"]
+
 print(results_PPI)
-print(results_SHOCK)
